832647d93c0053864dad155a2ae21153e457a3c8
mrvin100 <vincentyoumssi@example.com>
2025-01-17 18:44:33 +0100

    fix sutup problem

--- components/home/calendar-form.tsx
+++ components/home/calendar-form.tsx
@@ -1,14 +1,14 @@
-"use client"
+"use client";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { format } from "date-fns"
-import { CalendarIcon } from "lucide-react"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { Calendar } from "@/components/ui/calendar"
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
 import {
   Form,
   FormControl,
@@ -16,55 +16,68 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
+} from "@/components/ui/form";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from "@/components/ui/popover"
-import { toast } from "sonner"
-import { FC } from "react"
+} from "@/components/ui/popover";
+import { toast } from "sonner";
+import { FC } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 const FormSchema = z.object({
-  doci: z.date({
+  doci: z.string({
     required_error: "An event is required.",
   }),
   doco: z.date({
     required_error: "A date of event is required.",
   }),
-})
+});
 
-export const CalendarForm:FC = () => {
+export const CalendarForm: FC = () => {
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
-  })
+  });
 
   function onSubmit(data: z.infer<typeof FormSchema>) {
-    toast("You submitted the following values:",{
+    toast("You submitted the following values:", {
       description: (
         <pre className="mt-2 w-full max-w-[340px] rounded-md bg-slate-950 p-4">
           <code className="text-white">{JSON.stringify(data, null, 2)}</code>
         </pre>
       ),
-    })
+    });
   }
-  const day = new Date()
-  const currentDate = `${day.getFullYear()}-${day.getMonth() + 1}-${day.getDate()}`
-  
+  const day = new Date();
+  const currentDate = `${day.getFullYear()}-${
+    day.getMonth() + 1
+  }-${day.getDate()}`;
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 grid grid-cols-2 md:grid-cols-4 items-end justify-center gap-4">
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 grid grid-cols-2 md:grid-cols-4 items-end justify-center gap-4"
+      >
         <FormField
           control={form.control}
           name="doci"
           render={({ field }) => (
             <FormItem className="inline-grid">
-              <FormLabel className="uppercase text-xs font-light">choose an event</FormLabel>
-              <Popover>
-                <PopoverTrigger asChild>
-                  <FormControl>
-                    <Button
+              <FormLabel className="uppercase text-xs font-light">
+                choose an event
+              </FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    {/* <Button
                       variant={"outline"}
                       className={cn(
                         " pl-3 text-left font-normal py-7 text-xs",
@@ -77,21 +90,16 @@ export const CalendarForm:FC = () => {
                         <span>Pick an event</span>
                       )}
                       <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                    </Button>
-                  </FormControl>
-                </PopoverTrigger>
-                <PopoverContent className="w-auto p-0" align="start">
-                  <Calendar
-                    mode="single"
-                    selected={field.value ?? currentDate}
-                    onSelect={field.onChange}
-                    disabled={(date) =>
-                      date > new Date() || date < new Date("1900-01-01")
-                    }
-                    initialFocus
-                  />
-                </PopoverContent>
-              </Popover>
+                    </Button> */}
+                    <SelectValue placeholder="Select a verified email to display" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="m@example.com">m@example.com</SelectItem>
+                  <SelectItem value="m@google.com">m@google.com</SelectItem>
+                  <SelectItem value="m@support.com">m@support.com</SelectItem>
+                </SelectContent>
+              </Select>
               <FormMessage />
             </FormItem>
           )}
@@ -101,7 +109,9 @@ export const CalendarForm:FC = () => {
           name="doco"
           render={({ field }) => (
             <FormItem className="inline-grid">
-              <FormLabel className="uppercase text-xs font-light">date</FormLabel>
+              <FormLabel className="uppercase text-xs font-light">
+                date
+              </FormLabel>
               <Popover>
                 <PopoverTrigger asChild>
                   <FormControl>
@@ -142,7 +152,9 @@ export const CalendarForm:FC = () => {
           name="doco"
           render={({ field }) => (
             <FormItem className="inline-grid">
-              <FormLabel className="uppercase text-xs font-light">Guests:</FormLabel>
+              <FormLabel className="uppercase text-xs font-light">
+                Guests:
+              </FormLabel>
               <Popover>
                 <PopoverTrigger asChild>
                   <FormControl>
@@ -178,8 +190,13 @@ export const CalendarForm:FC = () => {
             </FormItem>
           )}
         />
-        <Button type="submit" className="inline-flex py-7 text-card-foreground font-normal text-xs uppercase tracking-wider">check avalaibility</Button>
+        <Button
+          type="submit"
+          className="inline-flex py-7 text-card-foreground font-normal text-xs uppercase tracking-wider"
+        >
+          check avalaibility
+        </Button>
       </form>
     </Form>
-  )
-}
+  );
+};
